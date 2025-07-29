55a2f51efcfc0b42a4895a229823f8af1b8a0f16
sarojasahoo <saroja.svmsahoo@gmail.com>
2025-07-29 11:38:24 +0100

    first commit

---
+++ src/main/java/com/di/streamnova/StreamNovaApplication.java
@@ -0,0 +1,13 @@
+package com.di.streamnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamNovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamNovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/di/streamnova/StreamNovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.di.streamnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamNovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
